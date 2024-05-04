a90d30256d02f642b4d12173f4ce8feec143e7e3
iot-bot-11 <166243820+iot-bot-11@users.noreply.github.com>
2024-05-05 02:21:04 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,7 +36,7 @@ if "chat_session" not in st.session_state:
 
 
 # Display the chatbot's title on the page
-st.title("🤖 vit Pro - ChatBot")
+st.title("🤖 Be truthful to vita ")
 
 # Display the chat history
 for message in st.session_state.chat_session.history:
